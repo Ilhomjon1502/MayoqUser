a6b046131c1da4c088b03f691a6b24f8d13594c5
saydullo <abdurashidovsaydullo35@gmail.com>
2023-06-02 18:38:20 +0500

    HomeFragment is finished.!

---
+++ app/src/main/java/uz/ilhomjon/soscaruser/view/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package uz.ilhomjon.soscaruser.view.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.ilhomjon.soscaruser.R
+import uz.ilhomjon.soscaruser.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return binding.root
+    }
+}
