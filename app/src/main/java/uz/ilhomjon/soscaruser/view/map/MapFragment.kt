daab89fe19a1406e2f2cdbf011a3ab01e3b6a4e8
saydullo <abdurashidovsaydullo35@gmail.com>
2023-06-11 10:14:29 +0500

    AddMap

--- app/src/main/java/uz/ilhomjon/soscaruser/view/map/MapFragment.kt
+++ app/src/main/java/uz/ilhomjon/soscaruser/view/map/MapFragment.kt
@@ -1,60 +1,169 @@
 package uz.ilhomjon.soscaruser.view.map
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import uz.ilhomjon.soscaruser.R
+import uz.ilhomjon.soscaruser.databinding.FragmentMapBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding by lazy { FragmentMapBinding.inflate(layoutInflater) }
+    private val TAG = "MapFragment"
+    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var LOCATION_PERMISSION_REQUEST_CODE: Int = 1
+    private var GPS_PERMISSION_REQUEST_CODE: Int = 2
+    private lateinit var getLocation: LatLng
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+    ): View {
+
+        showEnableGPSDialog()
+        requestLocationPermission()
+
+        val mapFragment= requireActivity().supportFragmentManager
+            .findFragmentById(R.id.mapContainer) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                binding.root.context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Lokatsiya ruxsatini so'ragan dialogni ko'rsatish
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                AlertDialog.Builder(context)
+                    .setTitle("Lokatsiya ruxsatini so'raymiz")
+                    .setMessage("Bu ilovadan foydalanish uchun lokatsiya ruxsatini so'ramiz.")
+                    .setPositiveButton("OK") { _, _ ->
+                        // Ruxsat so'roqni boshqarish
+                        ActivityCompat.requestPermissions(
+                            requireActivity(),
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            LOCATION_PERMISSION_REQUEST_CODE
+                        )
+                    }
+                    .create()
+                    .show()
+            } else {
+                // Ruxsat so'roqni boshqarish
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+        } else {
+            // Lokatsiya ruxsati berilgan
+            getCurrentLocation()
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            GPS_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // Lokatsiya va GPS ruxsati berilgan
+                    getCurrentLocation()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Lokatsiya va GPS ruxsat berilmadi",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    val currentLocation = LatLng(location.latitude, location.longitude)
+                    Log.d(TAG, "getCurrentLocation: $currentLocation")
+                    getLocation = currentLocation
+                }
+            }
+    }
+
+
+    // GPSni yoqishni so'raydigan dialogni ochish
+    private fun showEnableGPSDialog() {
+        val dialogBuilder = AlertDialog.Builder(binding.root.context)
+        dialogBuilder.setMessage("GPSni yoqing")
+            .setCancelable(false)
+            .setPositiveButton("Sozlamalar") { _, _ ->
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+            .setNegativeButton("Bekor qilish") { dialog, _ ->
+                dialog.cancel()
+            }
+
+        val dialog = dialogBuilder.create()
+        dialog.show()
+    }
+
+
+    private fun addMarkersToMap(googleMap: GoogleMap, currentLocation: LatLng) {
+
+    }
+
+
+    override fun onMapReady(p0: GoogleMap) {
+        googleMap = p0
+
+        // Set initial camera position (optional)
+        val initialPosition = LatLng(37.7749, -122.4194) // San Francisco
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialPosition, 12f))
+
+        // Add marker to the map
+        val markerPosition = LatLng(37.7749, -122.4194)
+        googleMap.addMarker(MarkerOptions().position(markerPosition).title("Marker Title"))
+
     }
 }
